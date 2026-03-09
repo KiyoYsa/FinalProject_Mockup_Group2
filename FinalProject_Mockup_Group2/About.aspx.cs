112555cd50bcfd6da50fa393e28b4c0156872997
Kris <Kris@LAPTOP-L340RQ83>
2026-03-09 10:22:49 +0800

    Add project files.

---
+++ FinalProject_Mockup_Group2/About.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinalProject_Mockup_Group2
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FinalProject_Mockup_Group2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalProject_Mockup_Group2.Global" Language="C#" %>
